c514a3144354a4b9c93db85146276990375ad3e6
Артём <Artem_rusan@mail.ru>
2023-03-10 01:42:13 +0300

    Синонимы

---
+++ main.py
@@ -0,0 +1,38 @@
+with open('synonyms.txt', 'r') as f:
+    read = f.read()
+
+    ListKey = []
+    ListValue = []
+
+    for line in read.split('\n'):
+        if not line.strip():
+            continue
+        k, v = [word.strip() for word in line.split('-')]
+
+        ListKey.append(k)
+        ListValue.append(v)
+
+for i, word1 in enumerate(ListKey):
+    ListKey[i] = word1.lower()
+for i, word1 in enumerate(ListValue):
+    ListValue[i] = ListValue[i].lower()
+
+print(ListKey)
+print(ListValue)
+
+WordID = input().lower()
+s = ''
+for i, word1 in enumerate(ListKey):
+    if WordID == word1:
+        print(ListValue[i].capitalize())
+    elif WordID == ListValue[i]:
+        print(ListKey[i].capitalize())
+
+s = input('Y/N')
+with open('synonyms.txt', 'a') as f:
+    if s == 'N':
+        newSin = input("Введите:")
+        f.write(f'{WordID} - {newSin}' + '\n')
+    else:
+        exit()
+
